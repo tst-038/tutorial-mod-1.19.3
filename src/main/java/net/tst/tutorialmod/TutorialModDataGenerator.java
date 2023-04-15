458ed21cf5167faf6681579f42d13f0f3b352291
tst-038 <tristan.degraeve@gmail.com>
2023-04-15 23:05:26 +0200

    Added TutorialModClient.java Changed ExampleMod.java and ExampleModDataGenerator.java to TutorialMod.java and TutorialModDataGenerator.java

--- src/main/java/net/tst/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/tst/tutorialmod/TutorialModDataGenerator.java
@@ -3,7 +3,7 @@ package net.tst.tutorialmod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
